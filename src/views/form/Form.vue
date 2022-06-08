e769c95997e4c312dbc6550803825c862c2833b4
undefined <undefined>
2022-06-08 10:03:48 +0800

    form表单调整

--- src/views/form/Form.vue
+++ src/views/form/Form.vue
@@ -11,31 +11,66 @@
       <h3>基本信息</h3>
       <a-row :gutter="24">
         <a-col :span="12">
-          <a-form-model-item label="输入框" prop="name">
-            <a-input v-model="form.name" />
+          <a-form-model-item label="输入框" prop="input">
+            <a-input v-model="form.input" />
           </a-form-model-item>
         </a-col>
         <a-col :span="12">
-          <a-form-model-item label="下拉选项" prop="region">
-            <a-select
-              v-model="form.region"
-              placeholder="please select your zone"
-            >
-              <a-select-option value="shanghai"> Zone one </a-select-option>
-              <a-select-option value="beijing"> Zone two </a-select-option>
+          <a-form-model-item label="下拉选项" prop="select">
+            <a-select v-model="form.select" placeholder="please select">
+              <a-select-option value="A"> A </a-select-option>
+              <a-select-option value="B"> B </a-select-option>
+              <a-select-option :value="1"> 1 </a-select-option>
             </a-select>
           </a-form-model-item>
         </a-col>
       </a-row>
       <a-row :gutter="24">
         <a-col :span="12">
+          <a-form-model-item label="下拉多选">
+            <a-select
+              v-model="form.selectm"
+              mode="multiple"
+              placeholder="please select"
+            >
+              <a-select-option value="A"> A </a-select-option>
+              <a-select-option value="B"> B </a-select-option>
+              <a-select-option :value="1"> 1 </a-select-option>
+            </a-select>
+          </a-form-model-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-model-item label="多个输入框">
+            <a-row :gutter="12">
+              <a-col :span="12">
+                <a-select
+                  v-model="form.select1"
+                  placeholder="please select"
+                >
+                  <a-select-option value="A"> A </a-select-option>
+                  <a-select-option value="B"> B </a-select-option>
+                  <a-select-option :value="1"> 1 </a-select-option>
+                </a-select></a-col
+              >
+              <a-col :span="12">
+                <a-input v-model="form.input1" />
+              </a-col>
+            </a-row>
+          </a-form-model-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="12">
           <a-form-model-item label="日期选择">
-            <a-date-picker v-model="form.date" />
+            <a-date-picker v-model="form.date" valueFormat="YYYY-MM-DD" />
           </a-form-model-item>
         </a-col>
         <a-col :span="12">
           <a-form-model-item label="时间选择">
-            <a-time-picker v-model="form.date"></a-time-picker>
+            <a-time-picker
+              v-model="form.time"
+              valueFormat="HH:mm:ss"
+            ></a-time-picker>
           </a-form-model-item>
         </a-col>
       </a-row>
@@ -43,7 +78,11 @@
       <a-row :gutter="24">
         <a-col :span="12">
           <a-form-model-item label="单选">
-            <a-radio-group name="radioGroup" :default-value="1">
+            <a-radio-group
+              v-model="form.radio"
+              name="radioGroup"
+              :default-value="1"
+            >
               <a-radio :value="1"> A </a-radio>
               <a-radio :value="2"> B </a-radio>
               <a-radio :value="3"> C </a-radio>
@@ -53,7 +92,7 @@
         </a-col>
         <a-col :span="12">
           <a-form-model-item label="复选">
-            <a-checkbox-group>
+            <a-checkbox-group v-model="form.checkbox">
               <a-checkbox value="A"> A </a-checkbox>
               <a-checkbox value="B"> B </a-checkbox>
               <a-checkbox value="C"> C </a-checkbox>
@@ -66,12 +105,17 @@
       <a-row :gutter="24">
         <a-col :span="12">
           <a-form-model-item label="多行文本">
-            <a-textarea placeholder="textarea with clear icon" allow-clear />
+            <a-textarea
+              v-model="form.textarea"
+              placeholder="textarea with clear icon"
+              allow-clear
+            />
           </a-form-model-item>
         </a-col>
         <a-col :span="12"></a-col>
       </a-row>
     </a-form-model>
+    {{ form }}
   </PageWrapper>
 </template>
 
@@ -87,18 +131,18 @@ export default class Form extends Vue {
   wrapperCol = { span: 18 }
 
   rules = {
-    name: [
+    input: [
       {
         required: true,
-        message: 'Please input Activity name',
+        message: 'Please input',
         trigger: 'blur'
       },
       { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' }
     ],
-    region: [
+    select: [
       {
         required: true,
-        message: 'Please select Activity zone',
+        message: 'Please select',
         trigger: 'change'
       }
     ]
